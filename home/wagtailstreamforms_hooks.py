54fc825b199f76eca59486c4a63dd384a3e33f08
rachelhs <rachelsmith400@hotmail.co.uk>
2021-02-11 12:16:32 +0000

    form has subject

--- home/wagtailstreamforms_hooks.py
+++ home/wagtailstreamforms_hooks.py
@@ -10,12 +10,8 @@ from decouple import config
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "rachel.development@gmail.com"  # Enter your address
-receiver_email = "rachelsmith400@hotmail.co.uk"  # Enter receiver address
+sender_email = "rachel.development@gmail.com"  # Enter your address, must be gmail
 password = config('EMAIL_PASSWORD')
-message = """\
-Subject: Hi there
-This message is sent from Python."""
 
 context = ssl.create_default_context()
 
@@ -24,9 +20,8 @@ def email_submission(instance, form):
     """ Send an email with the submission. """
 
     addresses = [instance.advanced_settings.to_address]
-    content = ['Please see below submission\n', ]
+    content = ['Subject: New Contact Form Submission\n', ]
     from_address = settings.DEFAULT_FROM_EMAIL
-    subject = 'New Form Submission : %s' % instance.title
 
     # build up the email content
     for field, value in form.cleaned_data.items():
@@ -37,22 +32,6 @@ def email_submission(instance, form):
             value = ', '.join(value)
         content.append('{}: {}'.format(field, value))
     content = '\n'.join(content)
-
-    # create the email message
-    email = EmailMessage(
-        body=content,
-        from_email=from_address,
-        to=addresses
-    )
-
-    # attach any files submitted
-    for field in form.files:
-        for file in form.files.getlist(field):
-            file.seek(0)
-            email.attach(file.name, file.read(), file.content_type)
-
-    # finally send the email
-    #email.send(fail_silently=True)
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
